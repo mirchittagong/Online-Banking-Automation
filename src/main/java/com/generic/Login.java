3656a4ae7bc87e92e5f77b3db69e360f54c75b87
Amin J <mirchittagong2020@gmail.com>
2023-11-16 20:57:57 -0500

    A

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,22 @@
+package com.generic;
+
+public class Login {
+	static int salary = 5000;
+	static String name = "Amin";
+	static double price = 2.75;
+	
+	//char address = 'Vestal';
+	
+	static Object a ="Amin";
+	
+	Login obj =new Login();
+	
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA");
+		System.out.println(salary);//Single variable
+		System.out.println(name);
+		System.out.println(price);
+		System.out.println(a);
+	}
+
+}
